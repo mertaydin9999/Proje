7c7c433cb6fa5b3ba65bc6b1363e39a54d9c31fc
mertaydin9999 <mertaydin9999@hotmail.com>
2021-03-19 14:30:36 +0300

    OOP-2

---
+++ Proje/Proje/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Proje
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public Command SelectedNoteChangedCommand { get; }
+        string selectedNote;
+        public string SelectedNote
+        {
+            get => selectedNote;
+            set
+            {
+                selectedNote = value;
+                var args = new PropertyChangedEventArgs(nameof(SelectedNote));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+        public MainPageViewModel()
+        {
+            Notes = new ObservableCollection<string>();
+            SelectedNoteChangedCommand = new Command(async () =>
+            {
+                var detailVM = new DetailPageViewModel(SelectedNote);
+                var detailPage = new DetailPage();
+                detailPage.BindingContext = detailVM;
+                await Application.Current.MainPage.Navigation.PushModalAsync(detailPage);
+            });
+
+            EraseNoteCommand = new Command(() =>
+            {
+                NoteText = string.Empty;
+            });
+
+            SaveNoteCommand = new Command(() =>
+            {
+                Notes.Add(NoteText);
+
+                NoteText = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> Notes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string noteText;
+        public string NoteText
+        {
+            get => noteText;
+            set
+            {
+                noteText = value;
+                var args = new PropertyChangedEventArgs( nameof(NoteText));
+                PropertyChanged?.Invoke(this,args);
+            }
+        }
+        public Command SaveNoteCommand { get; set; }
+        public Command EraseNoteCommand { get; }
+    }
+}
